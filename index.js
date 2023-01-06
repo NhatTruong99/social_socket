babcfb177d4087868160d8a06c1e3e5323496b8d
NhatTruong99 <79687800+NhatTruong99@users.noreply.github.com>
2023-01-06 17:48:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const io = require('socket.io')(8900,{
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
     cors:{
         origin:"http://localhost:3000",
     }
